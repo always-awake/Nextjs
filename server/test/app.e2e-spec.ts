d15b3e7d6253999ef3ea018b7073d6d492450f37
always-awake <leemirim9654@gmail.com>
2021-02-05 22:43:45 +0900

    service 로직 E2E 테스트 로직 추가 - post, delete, patch 로직 테스트 추가

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { INestApplication } from '@nestjs/common'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 import * as request from 'supertest'
 import { AppModule } from '../src/app.module'
 import { isRgbColor } from 'class-validator'
@@ -7,12 +7,17 @@ import { isRgbColor } from 'class-validator'
 describe('Test AppController (E2E)', () => {
   let app: INestApplication
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }))
     await app.init();
   })
 
@@ -31,7 +36,7 @@ describe('Test AppController (E2E)', () => {
       .expect([])
     })
 
-    it('Test POST', () => {
+    it('Test POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -42,10 +47,48 @@ describe('Test AppController (E2E)', () => {
         .expect(201)
     })
 
+    it('Test POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test Title',
+          year: 2000,
+          genres: ['Test Genres'],
+          other: 'thing',
+        })
+        .expect(400)
+    })
+
     it('Test DELETE', () => {
       return request(app.getHttpServer())
         .delete('/movies')
         .expect(404)
     })
   })
+
+  describe('Test /movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200)
+    })
+
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404)
+    })
+    it('PATCH', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Test' })
+        .expect(200)
+    })
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200)
+    })
+  })
+  
 })
